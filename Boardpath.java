24477f8a34e14439b86673ecd021140e0ca54fce
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-01-20 16:22:30 +0530

    Create Boardpath.java

---
+++ Boardpath.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+		int target=sc.nextInt();
+		int n=sc.nextInt();
+		harsh(n,target,"",0);
+		System.out.print("\n"+h);
+	}
+	static int h=0;
+	public static void harsh(int n,int target,String s,int sum){
+	    if(sum==target){
+            System.out.print(s+" ");
+            h++;
+	        return;
+	    }
+	    if(sum>target) return;
+	    for(int i=1;i<=n;i++){
+	        harsh(n,target,s+i,sum+i);
+	    }
+	}
+}
