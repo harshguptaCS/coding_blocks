30d16dca52b4c1407c1d1f995ea6a2d785856566
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-01-20 19:14:04 +0530

    Create Count_Remove_Replace_hi_Part-2.java

---
+++ Count_Remove_Replace_hi_Part-2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        harsh(s,"","");
+    }
+    static int h=0;
+    public static void harsh(String s,String s1,String s2){
+        if(s.length()<2){
+            System.out.println(h);
+            System.out.println(s1+s);
+            System.out.println(s2+s);
+            return;
+        }
+        if((s.charAt(0)=='h'&&s.charAt(1)=='i')&&(s.length()<3||s.charAt(2)!='t')){
+            h++;
+            harsh(s.substring(2),s1,s2+"bye");
+        }
+        else{
+            harsh(s.substring(1),s1+s.charAt(0),s2+s.charAt(0));
+        }
+    }
+}
