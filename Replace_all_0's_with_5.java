9ce31d98ad8ca3148b4c417dec2134e6796eed1b
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-01-25 22:59:26 +0530

    Update Replace_all_0's_with_5.java

--- Replace_all_0's_with_5.java
+++ Replace_all_0's_with_5.java
@@ -4,8 +4,17 @@ public class Main {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         String s=Integer.toString(n);
-        s=s.replace('0','5');
+        s=harsh(s,0,"");
         n=Integer.parseInt(s);
         System.out.print(n);
+    }
+    public static String harsh(String s,int i,String ans){
+        if(i==s.length()){
+            return ans;
+        }
+        if(s.charAt(i)=='0'){
+            return harsh(s,i+1,ans+"5");
+        }
+        else return harsh(s,i+1,ans+s.charAt(i));
     }
 }
